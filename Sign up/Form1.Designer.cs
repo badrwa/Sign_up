2a2bfe2f12d0fd62718cdd147bf642d08d87900e
badrwa <132658695+badrwa@users.noreply.github.com>
2023-11-23 20:59:30 +0100

    Ajoutez des fichiers projet.

---
+++ Sign up/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Sign_up
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtrm = new System.Windows.Forms.CheckBox();
+            this.txtcp = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtp = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txte = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtg = new System.Windows.Forms.ComboBox();
+            this.txtd = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtln = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtfn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtrm);
+            this.panel1.Controls.Add(this.txtcp);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.txtp);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txte);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txta);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtg);
+            this.panel1.Controls.Add(this.txtd);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtln);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtfn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(252, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(618, 603);
+            this.panel1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(253, 546);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(87, 29);
+            this.linkLabel1.TabIndex = 20;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Sign in";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(166, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 47);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Sign Up";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtrm
+            // 
+            this.txtrm.AutoSize = true;
+            this.txtrm.Location = new System.Drawing.Point(344, 447);
+            this.txtrm.Name = "txtrm";
+            this.txtrm.Size = new System.Drawing.Size(140, 24);
+            this.txtrm.TabIndex = 18;
+            this.txtrm.Text = "Remember Me";
+            this.txtrm.UseVisualStyleBackColor = true;
+            this.txtrm.CheckedChanged += new System.EventHandler(this.txtrm_CheckedChanged);
+            // 
+            // txtcp
+            // 
+            this.txtcp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcp.Location = new System.Drawing.Point(344, 392);
+            this.txtcp.Name = "txtcp";
+            this.txtcp.Size = new System.Drawing.Size(239, 35);
+            this.txtcp.TabIndex = 17;
+            this.txtcp.UseSystemPasswordChar = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(339, 360);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(210, 29);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Confirm Password";
+            // 
+            // txtp
+            // 
+            this.txtp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtp.Location = new System.Drawing.Point(344, 306);
+            this.txtp.Name = "txtp";
+            this.txtp.Size = new System.Drawing.Size(239, 35);
+            this.txtp.TabIndex = 15;
+            this.txtp.UseSystemPasswordChar = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(339, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 29);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Password";
+            // 
+            // txte
+            // 
+            this.txte.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txte.Location = new System.Drawing.Point(344, 225);
+            this.txte.Name = "txte";
+            this.txte.Size = new System.Drawing.Size(239, 35);
+            this.txte.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(339, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 29);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Email";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(344, 143);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(239, 35);
+            this.txta.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(339, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Address";
+            // 
+            // txtg
+            // 
+            this.txtg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtg.FormattingEnabled = true;
+            this.txtg.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.txtg.Location = new System.Drawing.Point(31, 392);
+            this.txtg.Name = "txtg";
+            this.txtg.Size = new System.Drawing.Size(239, 37);
+            this.txtg.TabIndex = 9;
+            // 
+            // txtd
+            // 
+            this.txtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtd.Location = new System.Drawing.Point(31, 306);
+            this.txtd.Name = "txtd";
+            this.txtd.Size = new System.Drawing.Size(239, 35);
+            this.txtd.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Day Birth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Gender";
+            // 
+            // txtln
+            // 
+            this.txtln.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtln.Location = new System.Drawing.Point(31, 225);
+            this.txtln.Name = "txtln";
+            this.txtln.Size = new System.Drawing.Size(239, 35);
+            this.txtln.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name";
+            // 
+            // txtfn
+            // 
+            this.txtfn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfn.Location = new System.Drawing.Point(31, 143);
+            this.txtfn.Name = "txtfn";
+            this.txtfn.Size = new System.Drawing.Size(239, 35);
+            this.txtfn.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign Up";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sign_up.Properties.Resources.R;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1258, 680);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtfn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox txtg;
+        private System.Windows.Forms.DateTimePicker txtd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtln;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcp;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtp;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txte;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox txtrm;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Sign up/Form1.cs
@@ -0,0 +1,105 @@
+﻿using ADODB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sign_up
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=LoginDb;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtfn.Text!="" && txtln.Text != "" && txtd.Text != "" && txtg.Text != "" && txta.Text != "" &&
+                    txte.Text != "" && txtp.Text != "" && txtcp.Text != "" )
+                {
+                    if (txtp.Text==txtcp.Text)
+                    {
+                        int v = check(txte.Text);
+                        if (v !=1)
+                        {
+                            connection.Open();
+                            SqlCommand command = new SqlCommand("insert into RegistrationTbl values(@f_name,@l_name," +
+                                "@b_date,@gender,@address,@email,@password)", connection);
+                            command.Parameters.AddWithValue("@f_name", txtfn.Text);
+                            command.Parameters.AddWithValue("@l_name", txtln.Text);
+                            command.Parameters.AddWithValue("@b_date", Convert.ToDateTime(txtd.Text));
+                            command.Parameters.AddWithValue("@gender", txtg.Text);
+                            command.Parameters.AddWithValue("@address", txta.Text);
+                            command.Parameters.AddWithValue("@email", txte.Text);
+                            command.Parameters.AddWithValue("@password", txtp.Text);
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                            MessageBox.Show("Register Succesfully!");
+                            txtfn.Text = "";
+                            txtln.Text = "";
+                            txtg.Text = "";
+                            txta.Text = "";
+                            txte.Text = "";
+                            txtp.Text = "";
+                            txtcp.Text = "";
+
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("You are already registered"); 
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("The password does not match");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Fill in the blinks!");
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        int check(string email)
+        {
+            connection.Open();
+            string query = "select count(*) from RegistrationTbl where email='" + email + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            int v=(int)command.ExecuteScalar();
+            connection.Close();
+            return v;
+
+        }
+
+        private void txtrm_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtrm.Checked)
+            {
+                txtp.UseSystemPasswordChar = false;
+                txtcp.UseSystemPasswordChar = false;
+                
+            }
+            else
+            {
+                txtp.UseSystemPasswordChar = true;
+                txtcp.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
